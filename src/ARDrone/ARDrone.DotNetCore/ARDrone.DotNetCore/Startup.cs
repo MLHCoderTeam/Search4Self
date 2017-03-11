78542e23f938fb8ac712330e5ba37f0bb3c21e7d
Claudiu Codoban <codobanclaudiu@gmail.com>
2017-03-11 11:15:35 +0000

    Added the twilio config and controllers to be able to send sms

--- src/ARDrone/ARDrone.DotNetCore/ARDrone.DotNetCore/Startup.cs
+++ src/ARDrone/ARDrone.DotNetCore/ARDrone.DotNetCore/Startup.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ARDrone.DotNetCore.Config;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Twilio;
 
 namespace ARDrone.DotNetCore
 {
@@ -27,15 +30,21 @@ namespace ARDrone.DotNetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions();
+
+            services.Configure<TwilioConfig>(Configuration.GetSection(nameof(TwilioConfig)));
+
             // Add framework services.
             services.AddMvcCore();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IOptions<TwilioConfig> twilioConfig)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            TwilioClient.Init(twilioConfig.Value.AccountSid, twilioConfig.Value.AuthToken);
 
             app.UseMvc();
         }
